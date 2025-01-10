a730bc6bc3b3643ec62d4aeb9a38b631717b9cd5
CaGListRo <paco.nyns@gmail.com>
2025-01-10 12:36:46 +0100

    WIP: Added the perf_counter to calculate delta time. Imported the needed random modules and tested the uniform.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from utils import load_images
 
 import pygame as pg
 
+from random import uniform, randint, choice
+from time import perf_counter
 from typing import Final
 
 
@@ -24,19 +26,29 @@ def event_handler() -> None | bool:
         if event.type == pg.QUIT:
             return True
         
-def draw_window(screen) -> None:
+def draw_window(screen: pg.display, images: list[pg.Surface], dt: float) -> None:
     """ Draws the window. """
-    screen.fill((0, 0, 0))
+    screen.fill((100, 100, 100))
+    # draw wig
+    pg.draw.rect(screen, (0, 0, 0), (395, 380, 10, 23), 5, 1)
+    # draw candle
     for i in range(100):
         pg.draw.line(screen, (255 - i * 2, 255 - i * 2, 255 - i * 2), (350 + i, 400), (350 + i, 600))
+    
+    # draw flame
+
 
     pg.display.update()
 
 if __name__ == "__main__":
+    old_time: float = perf_counter()
     while not stop:
         clock.tick(FPS)
+        dt: float = perf_counter() - old_time
+        old_time = perf_counter()
         stop = event_handler()
-        draw_window(screen)
+        draw_window(screen, images, dt)
+        print(uniform(0.5, 1.0))
 
 
 
